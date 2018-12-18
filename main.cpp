e13949624fc1f5f2ad509ec96bdbfc7fbbed26a9
Jason Alaee <jalaee8416@hwdsb.on.ca>
2018-12-18 09:38:52 -0500

    18/12/2018

--- main.cpp
+++ main.cpp
@@ -17,57 +17,74 @@
 #include <chrono>
 #include <windows.h>
 #include <windef.h>
-#include <graphics.h>
 #include <direct.h>
+#include <iomanip>
+#include <unordered_set>
+#include <map>
 using namespace std;
 
-void animations () {
-     int x,y,i;
-	int g=DETECT,d;
-       initgraph(&g,&d,"\tc\bgi");
-       cleardevice();
-       x=getmaxx()/2;
-       y=getmaxy()/2;
-       settextstyle(TRIPLEX_FONT, HORIZ_DIR, 3);
-       setbkcolor(rand());
-       setcolor(4);
-       outtextxy(30,100,"Press");
-       outtextxy(30,130,"any");
-       outtextxy(30,160,"key");
-       outtextxy(30,190, "to");
-       outtextxy(30,220,"Quit");
-       while (!kbhit())
-       {
-       setcolor(rand());
-       for (int i=0;i<50;i++)
-       circle(x,y,i );
-       setcolor(rand());
-       for (int j=70;j<120;j++)
-       circle(x,y,j);
-       setcolor(rand());
-       for (int k=140;k<190;k++)
-       circle(x,y,k);
-       setcolor(rand());
-       for (int l=210;l<230;l++)
-       circle(x,y,l);
-       delay(200);
-       }
-       getch();
-       closegraph();
+map<int, string> toys = {
+    { 1, "Teddy bear" },
+    { 2, "Toy blocks" },
+    { 3, "Rubber ducky" },
+    { 4, "Firetruck" },
+    { 5, "Hockey stick" },
+    { 6, "Book" },
+    { 7, "Toy car" },
+    { 8, "Slime" },
+    { 9, "Lego" },
+    { 10, "Pyjamas" }
+};
+
+class User {
+private:
+     //name of elf
+     string *name;
+     //date of hire in unix time
+     int *hireDate;
+     //years since hire
+     int *yearsSinceHire;
+     //toy assigned (see lookup table)
+     int *toyAssigned;
+     //number of toys made
+     int *toysMade;
+     //percent progress (0-100)
+     int *progress;
+
+public:
+     //init User
+     User (string *name, int *hireDate) {
+
+     }
+     //get name
+     //string *getName() {
+     //     return *name;
+     //}
+
+};
+
+
+int generateHash(string *username, string *password) {
+     string s(*username+*password);
+     hash<string> hash_fn;
+
+     size_t hash = hash_fn(s);
+
+     cout << hash << '\n'<<"hi";
+
+     return 
 }
 
-void animation2() {
-     int gd = DETECT, gm;
-   int x = 320, y = 240, radius;
-
-   initgraph(&gd, &gm, "C:\\TC\\BGI");
-
-   for ( radius = 25; radius <= 125 ; radius = radius + 20)
-      circle(x, y, radius);
-
-   getch();
-   closegraph();
+bool compareHash(int *hash, string *password, string *username) {
+     return generateHash(password, username) == *hash;
 }
+
+void password (){
+
+  cout << "same hashes:\n" << boolalpha;
+  cout << "nts1 and nts2: " << (ptr_hash(nts1)==ptr_hash(nts2)) << '\n';
+  cout << "str1 and str2: " << (==str_hash(str2)) << '\n';
+}*/
 
 void mouseInput ()
 {
@@ -91,9 +108,20 @@ void changeColor(int foreground, int background) {
 
 int main()
 {
-     changeColor(5, 6);
+     //string /***/username= /*new (nothrow) string (*/"Santa"/*)*/;
+     //string /***/password= /*new (nothrow) string (*/"hi"/*)*/;
 
-     animations2();
+     string *username = new string("jason");
+     string *password = new string("fu");
+
+     //cout<<generateHash(username, password);
+
+     changeColor(5, 6);
+     generateHash(username, password);
+
+     //animations2();
+
+     //password();
 
      setScreenPosAndSize();
 
