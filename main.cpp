1f24b10641ad56892343f9ee4ac47cb952174f34
Jason Amri <jasonamri@gmail.com>
2019-01-07 11:04:53 -0500

    2019/01/07

--- main.cpp
+++ main.cpp
@@ -27,6 +27,9 @@ using namespace std;
 #define SCREENSIZEHEIGHT 720
 
 
+#define NUMOFTOYS 10
+
+
 map<int, string> toys = {
     { 1, "Teddy bear" },
     { 2, "Toy blocks" },
@@ -45,8 +48,7 @@ void delay(long unsigned int millis)
 	//start timer
 	clock_t start = clock();
 	//check is time has elasped
-	while ((start + ((float)(millis / 1000) * CLOCKS_PER_SEC)) > (unsigned)clock())
-		;
+	while ((start + ((float)(millis / 1000) * CLOCKS_PER_SEC)) > (unsigned)clock());
 }
 
 /*
@@ -85,23 +87,43 @@ public:
 };
 
 
+class Child {
+private:
+     //name of child
+     string *name;
+     //birthday
+     int *birthday;
+     //age
+     int *age;
+     //gender
+     bool *gender;
+     //home address
+     string *homeAddress;
+     //number of siblings
+     int *numberOfSiblings;
+     //cookie rating
+     int *cookieRating;
+     //niceness rating
+     int *nicenessRating
+     //toy assinged
+     int *toyAssigned;
+
+public:
+     //constructor
+     Child (string *name, int *birthday, bool *gender, string *homeAddress, int *numberOfSiblings) {
+
+     }
+
+
+}
+
 size_t generateHash(string *username, string *password) {
      hash<string> hash_fn;
      return  hash_fn(*username+*password);
 }
 
 
-/*
-bool compareHash(size_t *hash, string *password, string *username) {
-    return generateHash(password, username) == *hash;
-}*/
-/*
-void password (){
 
-  cout << "same hashes:\n" << boolalpha;
-  cout << "nts1 and nts2: " << (ptr_hash(nts1)==ptr_hash(nts2)) << '\n';
-  cout << "str1 and str2: " << (==str_hash(str2)) << '\n';
-}*/
 
 POINT mouseInput ()
 {
@@ -174,6 +196,7 @@ int login(string *username, string *password) {
      size_t *hashToTest = new size_t;
      int *permissionLevel = new int;
 
+     //open login file
      if (loginFile->is_open()) {
           *loginFile>> *numberOfUsers;
 
@@ -195,10 +218,11 @@ int login(string *username, string *password) {
           if (generateHash(username, password)==*hashToTest) {
                return *permissionLevel;
           } else {
+               //incorrect password
                return 0;
           }
 
-
+     //can't open file
      } else {
           return -1;
      }
@@ -206,20 +230,39 @@ int login(string *username, string *password) {
 
 }
 
+void workshopMenu() {
+     //ascii art?
+
+
+}
 
 
 
+//elf
+void elfMenu(){
 
-
-void menu1(){
-
-in the workshop menu
+int *selectionelf = new (nothrow) int(10);
+/*in the workshop menu
 
 add new child
 view/edit child
 update niceness rating
 
-change password
+change password*/
+
+cout << " .----------------.  .----------------.  .----------------.   .----------------.  .----------------.  .-----------------. .----------------. " << endl;
+cout << "| |  _________   | || |   _____      | || |  _________   | | | | ____    ____ | || |  _________   | || | ____  _____  | || | _____  _____ | |" << endl;
+cout << "| | |_   ___  |  | || |  |_   _|     | || | |_   ___  |  | | | ||_   \\  /   _|| || | |_   ___  |  | || ||_   \\|_   _| | || ||_   _||_   _|| |" << endl;
+cout << "| |   |  _|  _   | || |    | |   _   | || |   |  _|      | | | |  | |\\  /| |  | || |   |  _|  _   | || |  | |\\ \\| |   | || |  | \'    \' |  | |" << endl;
+cout << "| |   | |_  \\_|  | || |    | |       | || |   | |_  \\_|  | | | |  |   \\/   |  | || |   | |_  \\_|  | || |  |   \\ | |   | || |  | |    | |  | |" << endl;
+cout << "| |  _| |___/ |  | || |   _| |__/ |  | || |  _| |_       | | | | _| |_\\/_| |_ | || |  _| |___/ |  | || | _| |_\   |_  | || |   \\ `--\' /   | |" << endl;
+cout << "| | |_________|  | || |  |________|  | || | |_____|      | | | ||_____||_____|| || | |_________|  | || ||_____|\\____| | || |    `.__.\'    | |" << endl;
+cout << "| |              | || |              | || |              | | | |              | || |              | || |              | || |              | |" << endl;
+cout << "| \'--------------\' || \'--------------\' || \'--------------\' | | \'--------------\' || \'--------------\' || \'--------------\' || \'--------------\' |" << endl;
+cout << " \'----------------\'  \'----------------\'  \'----------------\'   \'----------------\'  \'----------------\'  \'----------------\'  \'----------------\' " << endl;
+cout << "\t\t\t\t";
+
+
 
 
 }
@@ -265,13 +308,140 @@ admin() {
 
 
 
+adduser() {
+     /*string *username = new string("santa");
+     string *password = new string("santa");
+
+
+     cout<<generateHash(username, password);*/
+}
+
+void setupConsole() {
+     //setup console inorder to handle mouse clicks
+     HANDLE input_handle;
+     input_handle = GetStdHandle(STD_INPUT_HANDLE);
+     SetConsoleMode(input_handle, ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_INSERT_MODE | ENABLE_EXTENDED_FLAGS | ENABLE_MOUSE_INPUT);
+
+     setScreenPosAndSize();
+
+}
+
+int loadFiles(vector<User> users, vector<Child> children, int toys[10][2]) {
+
+     //load all users
+     ifstream *usersFile = new ifstream;
+     usersFile->open("users.txt");
+
+     int *numberOfUsers = new int;
+
+     string *name = new string;
+     int *hireDate = new int;
+     int *toyAssigned = new int;
+     int *numberOfToysMade = new int;
+
+     //open user file
+     if (usersFile->is_open()) {
+          *usersFile>> *numberOfUsers;
+
+          for (int i = 0; i < *numberOfUsers; i++) {
+
+               *usersFile >> *name;
+               *usersFile >> *hireDate;
+               *usersFile >> *toyAssigned;
+               *usersFile >> *numberOfToysMade;
+
+               User tempUser(name, hireDate, toysAssigned, numberOfToysMade);
+
+               users.push_back(tempUser);
+
+               delete tempUser();
+
+          }
+
+          usersFile->close();
+
+     }
+     //end load all users
+
+
+     //load children
+     ifstream *childrensFile = new ifstream;
+     childrensFile->open("users.txt");
+
+     int *numberOfUsers = new int;
+
+     string *name = new string;
+     int *birthday = new int;
+     bool *gender = new bool;
+     string *homeAddress = new string;
+     int *numberOfSiblings = new int;
+
+     //open user file
+     if (childrensFile->is_open()) {
+          *childrensFile>> *numberOfChildren;
+
+          for (int i = 0; i < *numberOfChildren; i++) {
+
+               *usersFile >> *name;
+               *usersFile >> *birthday;
+               *usersFile >> *gender;
+               *usersFile >> *homeAddress;
+               *usersFile >> *numberOfSiblings;
+
+               Child tempChild(name, birthday, gender, homeAddress, numberOfSiblings);
+
+               child.push_back(tempChild);
+
+               delete tempChild();
+          }
+          childrensFile->close();
+     }
+     //end load children
+
+     //load toys
+     ifstream *toysFile = new ifstream;
+     toysFile->open("toys.txt");
+
+     int *needed = new int;
+     int *made = new int;
+
+
+     if (toysFile->is_open()) {
+
+          for (int i = 0; i < NUMOFTOYS; i++) {
+
+               *toysFile >> *needed;
+               *toysFile >> *made;
+
+               toys[i][0]=*needed;
+               toys[i][1]=*made;
+          }
+          toysFile->close();
+     }
+     //end load toys
+
+}
+
+/*todo
+add user function
+classes for users and children
+
+*/
+
+void error() {
+
+}
+
 int main()
 {
+
+     setupConsole();
 
 
      welcome();
 
 
+     //handle login
      int *permission = new int;
 
      string *username = new string;
@@ -283,55 +453,70 @@ int main()
      cin>>*password;
 
 
-     *permission = login();
+     *permission = login(username, password);
 
      while(*permission == 0) {
-
-
-
-          *permission = login();
+          //Todo: failed login try again
+          *permission = login(username, password);
      }
 
+
+     vector<User> users;
+     vector<Child> children;
+     int *toys = new int[10][2];
+
+     loadFiles(users, children, toys);
+
+
+/*
+     permission level
+     1: elf
+     2: ms. claus
+     3: mr. claus
+     4: admin
+*/
      switch(*permission) {
           case 1:
-               menu1();
+               elfMenu();
+               break;
+          case 2:
+               mrsClausMenu();
+               break;
+          case 3:
+               mrClausMenu();
+               break;
+          case 4:
+               admin();
+               break;
+          default:
+               error();
                break;
      }
 
-
-     string *username = new string("santa");
-     string *password = new string("santa");
-
-
-     cout<<generateHash(username, password);
 
      changeColor(5, 6);
 
 
 
-
-     //setup console inorder to handle mouse clicks
-     HANDLE input_handle;
-     input_handle = GetStdHandle(STD_INPUT_HANDLE);
-     SetConsoleMode(input_handle, ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_INSERT_MODE | ENABLE_EXTENDED_FLAGS | ENABLE_MOUSE_INPUT);
-
-
-     setScreenPosAndSize();
-
-
-
-
-
-
-
-
-
-
-     //animations2();
-
-     //while (true)
-     //cout<<clicked(100, 150, 200, 250)<<"\n"<<mouseInput().x<<" "<<mouseInput().y<<"\n";
-
      _getch();
      return EXIT_SUCCESS;
 }
+
+
+//code snippets
+//animations2();
+
+//while (true)
+//cout<<clicked(100, 150, 200, 250)<<"\n"<<mouseInput().x<<" "<<mouseInput().y<<"\n";
+
+/*
+bool compareHash(size_t *hash, string *password, string *username) {
+    return generateHash(password, username) == *hash;
+}*/
+/*
+void password (){
+
+  cout << "same hashes:\n" << boolalpha;
+  cout << "nts1 and nts2: " << (ptr_hash(nts1)==ptr_hash(nts2)) << '\n';
+  cout << "str1 and str2: " << (==str_hash(str2)) << '\n';
+}*/
